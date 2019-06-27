ff9e72788c2a4af611af5cbfeead53a57e3b9359
NumbTyler <is0237kv@ed.ritsumei.ac.jp>
2019-06-27 18:52:58 +0900

    Database

--- beatcount/admin.py
+++ beatcount/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Answer
 from .models import rAnswer
+from .models import pAnswer
 
 admin.site.register(Answer)
 admin.site.register(rAnswer)
+admin.site.register(pAnswer)
 # Register your models here.
